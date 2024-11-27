863a4662809c82430bde846822ecc5af38f93940
Alperen <alperenmimarlar@gmail.com>
2024-11-27 23:18:57 +0300

    Task finished

---
+++ Java101/Pratik - Not Ortalaması Hesaplayan Program/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Matematik Notunuz:");
+        mat = input.nextInt();
+        System.out.println("Fizik Notunuz:");
+        fizik = input.nextInt();
+        System.out.println("Kimya Notunuz:");
+        kimya = input.nextInt();
+        System.out.println("Türkçe Notunuz:");
+        turkce = input.nextInt();
+        System.out.println("Tarih Notunuz:");
+        tarih = input.nextInt();
+        System.out.println("Müzik Notunuz:");
+        muzik = input.nextInt();
+        double toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6.0;
+
+        System.out.println("Ortalamanız"+sonuc);
+        System.out.println(sonuc < 60 ? "Kaldı" : "Geçti");
+
+    }
+
+}
