05e6c73e8893dcf84dab06b20f794246e014e664
Alperen <alperenmimarlar@gmail.com>
2024-11-28 22:56:41 +0300

    Merkez açısının ölçüsü girildi.

--- Java101/Pratik - Dairenin Alanını ve Çevresini Bulan Program/Main.java
+++ Java101/Pratik - Dairenin Alanını ve Çevresini Bulan Program/Main.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-    
-    int r;
-    double pi=3.14;
-    Scanner inp=new Scanner(System.in);
-    System.out.println("Dairenin yarıçapını giriniz : ");
-    r=inp.nextInt();
-    double alan=pi*r*r;
-    double cevre=2*pi*r;
-    System.out.println("Dairenin Alanı : " + alan);
-    System.out.println("Dairenin Çevresi : " + cevre);
-    inp.close();
-}
+
+        int r;
+        double pi = 3.14;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Dairenin yarıçapını giriniz : ");
+        r = inp.nextInt();
+
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Dairenin Alanı : " + alan);
+        System.out.println("Dairenin Çevresi : " + cevre);
+
+        System.out.println("Daire diliminin merkez açısını giriniz (𝛼) : ");
+        double alfa = inp.nextDouble();
+
+        double dilimAlan = (pi * (r * r) * alfa) / 360;
+
+        System.out.println("Daire diliminin Alanı : " + dilimAlan);
+
+        inp.close();
+    }
 }
