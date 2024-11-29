7c92f1f4428bd07f153b458d879d8cb9182ef7ea
Alperen <alperenmimarlar@gmail.com>
2024-11-29 20:23:19 +0300

    Added program that draws an inverted triangle.

---
+++ Java101/Ödev - Ters Üçgen Yapımı/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak Sayısı: ");
+        int n = input.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
