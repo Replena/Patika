9c49238a41e784c18d27d24eacc5d8503e11ddee
Alperen <alperenmimarlar@gmail.com>
2024-11-29 16:26:32 +0300

    A program was added that gives the average of numbers divided by 3 and 4 up to the entered number.

---
+++ Java101/Pratik - Girilen Sayıya Kadar Olan Çift Sayıları Bulan Program/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+        double sum = 0;
+        int count = 0;
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                sum += i;
+                count++;
+            }
+        }
+        if (count > 0) {
+            double average = sum / count;
+            System.out.println("3 ve 4'e tam bölünen sayıların ortalaması: " + average);
+        } else {
+            System.out.println("3 ve 4'e tam bölünen sayı bulunmamaktadır.");
+        }
+        scanner.close();
+    }
+}
