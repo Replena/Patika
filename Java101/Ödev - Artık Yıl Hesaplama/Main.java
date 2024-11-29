3d1c1b51257b989731201a7d9b11d8df933a3005
Alperen <alperenmimarlar@gmail.com>
2024-11-29 16:19:55 +0300

    Leap year calculation added

---
+++ Java101/Ödev - Artık Yıl Hesaplama/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yıl Giriniz: ");
+        int year = scanner.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " bir artık yıldır !");
+        } else {
+            System.out.println(year + " bir artık yıl değildir !");
+        }
+
+        scanner.close();
+    }
+}
