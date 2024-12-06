a0aebb9f8c979d2af159f4f81a2c665c733811f4
Alperen <alperenmimarlar@gmail.com>
2024-12-06 17:36:35 +0300

    Added a program to calculate the frequency of elements in an array

---
+++ Java101/Ödev - Dizideki Elemanların Frekansı/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 20, 10, 10, 20, 5, 20, 18, 7, 7};
+
+        int[] fr = new int[arr.length];
+        int visited = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    fr[j] = visited;
+                }
+            }
+
+            if (fr[i] != visited) {
+                fr[i] = count;
+            }
+        }
+
+        System.out.println("Tekrar Sayıları:");
+        for (int i = 0; i < arr.length; i++) {
+            if (fr[i] != visited) {
+                System.out.println(arr[i] + " sayısı " + fr[i] + " kere tekrar edildi.");
+            }
+        }
+    }
+}
